82d4f40189f0e85b750f2cd8c0e424ba94ea9365
John Collier <jmcollier139@gmail.com>
2015-02-26 01:02:36 -0500

    Added the rest of the files... oops

---
+++ communityfundproject/rango/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    return HttpResponse("Range says here is the about page")
+
+# Create your views here.
