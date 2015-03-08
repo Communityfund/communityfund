0a45376b1ce837025c1360701e2d7ec66de707e8
John Collier <jmcollier139@gmail.com>
2015-03-07 19:57:58 -0500

    More tutorial changes - DB stuff

--- communityfundproject/rango/views.py
+++ communityfundproject/rango/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
