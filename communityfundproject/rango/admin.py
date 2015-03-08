5a1c41e57e43fe5869383191966124fd19b7e261
John Collier <jmcollier139@gmail.com>
2015-03-07 20:55:27 -0500

    no message

--- communityfundproject/rango/admin.py
+++ communityfundproject/rango/admin.py
@@ -3,7 +3,7 @@ from rango.models import Category, Page
 
 # Add in this class to customized the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slugs':('name',)}
 
 # Update the registeration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
