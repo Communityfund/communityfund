1b86159fc5757c30fec4c3eba51714dbe24d7e77
John Collier <jmcollier139@gmail.com>
2015-03-07 20:58:02 -0500

    no message

--- communityfundproject/rango/admin.py
+++ communityfundproject/rango/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 # Update the registeration to include this customised interface
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category)
 admin.site.register(Page)
