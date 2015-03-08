2897f28cb0e1239825a69d4c4fc6897e65aaafc1
Ubuntu <ubuntu@ip-10-0-0-233.us-west-2.compute.internal>
2015-03-08 15:53:40 +0000

    Flushing repo

--- communityfundproject/rango/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page
-
-# Update the registeration to include this customised interface
-admin.site.register(Category)
-admin.site.register(Page)
