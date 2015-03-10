6bc3c6d1a764cf32e454edbba5efd6e1f3e6bf5e
John Collier <jmcollier139@gmail.com>
2015-03-10 13:01:19 -0400

    no message

--- communityfundproject/communityfund/admin.py
+++ communityfundproject/communityfund/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from communityfund.models import Communities, Users, Projects
+
+admin.site.register(Communities)
+admin.site.register(Users)
+admin.site.register(Projects)
 
 # Register your models here.
