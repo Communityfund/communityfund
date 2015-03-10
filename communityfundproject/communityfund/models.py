ab3d4132cf44f36182cb02926167824a05aa49f8
John Collier <jmcollier139@gmail.com>
2015-03-10 12:48:43 -0400

    no message

--- communityfundproject/communityfund/models.py
+++ communityfundproject/communityfund/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
-class Communities(models.Model):
-    community = models.CharField(max_length=32)
-    province = models.CharField(max_length=32)
+#class Communities(models.Model):
+#    community = models.CharField(max_length=32)
+#    province = models.CharField(max_length=32)
     
-    def __unicode__(self):
-        return self.name + ", " + self.province
+#    def __unicode__(self):
+#        return self.name + ", " + self.province
         
 class Users(models.Model):
     userName = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=32)
-    community = models.ForeignKey(Communities, limit_choices_to={'id'})
+    #community = models.ForeignKey(Communities, limit_choices_to={'id'})
     
     def __unicode__(self):
         return self.userName
 
 class Projects(models.Model):
     projectName = models.CharField(max_length=32, primary_key=True)
-    community = models.ForeignKey(Communities, limit_choices_to={'id'})
+    #community = models.ForeignKey(Communities, limit_choices_to={'id'})
     initiator = models.ForeignKey(Users, limit_choices_to={'userName'})
     goal = models.IntegerField()
     amountFunded = models.IntegerField()
