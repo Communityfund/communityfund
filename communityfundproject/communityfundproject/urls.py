de6a789279147f1cf68f3ed93f9c48f73134791d
John Collier <jmcollier139@gmail.com>
2015-03-11 12:38:47 -0400

    no message

--- communityfundproject/communityfundproject/urls.py
+++ communityfundproject/communityfundproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
     # Examples:
