21f35488679382c25558a64e75c6388b0900ee8d
John Collier <jmcollier139@gmail.com>
2015-03-12 19:19:53 -0400

    no message

--- communityfundproject/communityfund/urls.py
+++ communityfundproject/communityfund/urls.py
@@ -3,5 +3,5 @@ from communityfund import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^$', views.login, name='login'),
+        url(r'^login/$', views.login, name='login'),
         url(r'^about/', views.about, name='about'))
