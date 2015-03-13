7054375d4f01a81bf7606272d22279dad95fa7b5
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-03-13 19:46:24 -0400

    Update urls.py

--- communityfundproject/communityfund/urls.py
+++ communityfundproject/communityfund/urls.py
@@ -4,5 +4,9 @@ from communityfund import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^login/$', views.login, name='login'),
+        url(r'^projects/$', views.projects, name='projects'),
+        url(r'^topproject/$', views.topproject, name='topproject'),
+        url(r'^create/$', views.create, name='create'),
+        url(r'^createdetail/$', views.createdetail, name='createdetail'),
         url(r'^signup/$', views.signup, name='signup'),
         url(r'^about/$', views.about, name='about'))
