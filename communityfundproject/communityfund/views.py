c43defb2f4294b7e28037c46bb0d239326dae291
John Collier <jmcollier139@gmail.com>
2015-03-12 20:36:18 -0400

    Added the populating for communities dropdown box

--- communityfundproject/communityfund/views.py
+++ communityfundproject/communityfund/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Communities
 
 def index(request):
     context_dict = {'boldmessage': "We are powered by Django!"}
@@ -12,6 +13,7 @@ def login(request):
     return render(request, 'communityfund/login.html')
     
 def signup(request):
+    context_dict = {'communities': Communities.objects.all()}
     return render(request, 'communityfund/signup.html')
     
 # Create your views here.
