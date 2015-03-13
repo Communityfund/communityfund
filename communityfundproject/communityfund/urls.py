ee9c53932217d268066ab7cdd1e4db1fe96ec735
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-03-13 16:09:03 -0400

    Update urls.py

--- communityfundproject/communityfund/urls.py
+++ communityfundproject/communityfund/urls.py
@@ -5,5 +5,4 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^login/$', views.login, name='login'),
         url(r'^signup/$', views.signup, name='signup'),
-        url(r'^top-projects/$', views.top-projects, name='top-projects'),
         url(r'^about/$', views.about, name='about'))
